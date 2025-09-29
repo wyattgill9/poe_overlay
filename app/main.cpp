dc068cc458e7aa01cda9e2e7a1de19db5ad04b9a
Wyatt Gill <101477459+wyattgill9@users.noreply.github.com>
2025-09-28 18:17:20 -0700

    http

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,18 @@
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "httplib.h"
+
 #include "example.h"
 
 int main() {
     Test test;
     test.test();
+
+    // TODO: make https & use openssl
+    httplib::Client cli("http://mobalytics.gg/poe-2/builds/lightning-arrow-farmer-fubgun");
+    auto res = cli.Get("/");
+
+    std::cout<< res->status << "\n";
+    std::cout<< res->body << "\n";
 
     return 0;
 }
