6c5a2baf3e2139be07f994742f2cb58e03ac9c46
wyattgill9 <101477459+wyattgill9@users.noreply.github.com>
2025-10-03 10:30:03 -0700

    fix?

--- app/main.cpp
+++ app/main.cpp
@@ -7,13 +7,13 @@ int main() {
     PassiveTree tree;
 
     tree.add_node(0, "Hi");
-    
+     
     httplib::Client cli("https://mobalytics.gg");
     auto res = cli.Get("/poe-2/builds/lightning-arrow-farmer-fubgun");
 
     if(res) {
-    //     std::cout<< res->status << "\n";
-        std::cout<< res->body   << "\n";
+        // std::cout << res->status << "\n";
+        std::cout << res->body   << "\n";
     }
 
     return 0;
